a119489ddbd8afb3402c76705b5ef4edd4a536d0
avslav <66104364+avslav@users.noreply.github.com>
2021-05-04 19:23:23 -0400

    Create nested_ifelse.c

---
+++ Control Flow/nested_ifelse.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+
+int x=10, y=47;
+
+    if(x==10)
+    {
+        if(y==50)
+        {
+          printf("x = 10, and y = 50.");
+        }
+        
+        else {
+          printf("y is not 50");
+        }
+        
+    }
+    
+    else {
+      printf("x is not 10");
+    }
+}

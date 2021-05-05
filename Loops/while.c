716e0b1cfb9b274842954f0a5cba893150ac5bc4
avslav <66104364+avslav@users.noreply.github.com>
2021-05-04 20:01:39 -0400

    Create while.c

---
+++ Loops/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int n = 1; 
+  int iterations = 50;
+
+  while( n <= iterations)
+   {
+    printf("%d loops completed\n", n);
+    n++;
+   }
+ 
+   return 0;
+}
